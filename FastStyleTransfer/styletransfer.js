ce72740090d240cf839c1ec0c0de44debdf0e3e2
maybay21 <mbk38@cornell.edu>
2019-07-10 17:48:53 -0400

    add license to sketches

--- FastStyleTransfer/styletransfer.js
+++ FastStyleTransfer/styletransfer.js
@@ -1,3 +1,24 @@
+// Copyright (C) 2018 Runway ML Examples
+// 
+// This file is part of Runway ML Examples.
+// 
+// Runway-Examples is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// Runway-Examples is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with Runway AI Examples.  If not, see <http://www.gnu.org/licenses/>.
+// 
+// ===========================================================================
+// RUNWAY
+// www.ruwnayml.com
+
 let img;
 let raw = new Image();
 
